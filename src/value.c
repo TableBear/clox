7c73cee758778d5e3ab6445e664f309bbd28b03f
ZhuoXiongHuang <zhuoxionghuang@qq.com>
2025-01-19 22:21:54 +0800

    feat: add NaN Boxing optimization

--- src/value.c
+++ src/value.c
@@ -13,7 +13,7 @@ void initValueArray(ValueArray *array) {
 
 void writeValueArray(ValueArray *array, Value value) {
     if (array->capacity < array->count + 1) {
-        int oldCapacity = array->capacity;
+        const int oldCapacity = array->capacity;
         array->capacity = GROW_CAPACITY(oldCapacity);
         array->values = GROW_ARRAY(Value, array->values, oldCapacity, array->capacity);
     }
@@ -28,6 +28,20 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+    if (IS_BOOL(value)) {
+        printf(AS_BOOL(value) ? "true" : "false");
+    } else if (IS_NIL(value)) {
+        printf("nil");
+    } else if (IS_NUMBER(value)) {
+        printf("%g", AS_NUMBER(value));
+    } else if (IS_OBJ(value)) {
+        printObject(value);
+    } else {
+        printf("<unknown>");
+    }
+#else
+
     switch (value.type) {
         case VAL_BOOL:
             printf(AS_BOOL(value) ? "true" : "false");
@@ -42,9 +56,16 @@ void printValue(Value value) {
             printObject(value);
             break;
     }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+    if (IS_NUMBER(a) && IS_NUMBER(b)) {
+        return AS_NUMBER(a) == AS_NUMBER(b);
+    }
+    return a == b;
+#else
     if (a.type != b.type) return false;
     switch (a.type) {
         case VAL_BOOL:
@@ -57,4 +78,5 @@ bool valuesEqual(Value a, Value b) {
         default:
             return false;
     }
+#endif
 }
